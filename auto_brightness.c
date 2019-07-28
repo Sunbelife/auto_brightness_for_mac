ab21fccdb447b5d72bde6c75446354539f53b0b3
Sunbelife <Sunbelife@foxmail.com>
2019-07-28 15:47:39 +0800

    fix bug

--- auto_brightness.c
+++ auto_brightness.c
@@ -19,36 +19,37 @@ int main() {
 	
 	wiringPiSetup();
 	pcf8591Setup(A0, Address);
-
+	
 	while(1) {
-		read_num = analogRead(A0);
-		printf("目前读取亮度值：%d\n", read_num); //输出AD转换后的值 delay(1000);
-		if (read_num > max_num) {
-			if (status == 0) {
-				continue;
+			read_num = analogRead(A0);
+			printf("目前读取亮度值：%d\n", read_num); //输出AD转换后的值 delay(1000);
+			if (read_num < min_num) {
+					if (status == 0) {
+							continue;
+					}
+					printf("环境有点暗，正在调暗屏幕\n");
+					system("ssh sunbelife@192.168.123.141 /usr/local/bin/ddcctl -d 1 -b 1");
+					status = 0;
+					delay(3000);
+			} else if (read_num < max_num && read_num > min_num) {
+					if (status == 1) {
+							continue;
+					}
+					printf("环境亮度还中，把屏幕亮度调到中\n");
+					system("ssh sunbelife@192.168.123.141 /usr/local/bin/ddcctl -d 1 -b 50");
+					status = 1;
+					delay(3000);
+			} else if (read_num > max_num) {
+					if (status == 2) {
+							continue;
+					}
+					printf("环境太亮，屏幕亮度全开\n");
+					system("ssh sunbelife@192.168.123.141 /usr/local/bin/ddcctl -d 1 -b 100");
+					status = 2;
+					delay(3000);
 			}
-			printf("环境有点暗，正在调暗屏幕\n");
-			system("ssh sunbelife@192.168.123.141 /usr/local/bin/ddcctl -d 1 -b 1");
-			status = 0;
-			delay(3000);
-		} else if (read_num < max_num && read_num > min_num) {
-			if (status == 1) {
-				continue;
-			}
-			printf("环境亮度还中，把屏幕亮度调到中\n");
-			system("ssh sunbelife@192.168.123.141 /usr/local/bin/ddcctl -d 1 -b 50");
-			status = 1;
-			delay(3000);
-		} else if (read_num < min_num) {
-			if (status == 2) {
-				continue;
-			}
-			printf("环境太亮，屏幕亮度全开\n");
-			system("ssh sunbelife@192.168.123.141 /usr/local/bin/ddcctl -d 1 -b 100");
-			status = 2;
-			delay(3000);
+			delay(1000);
 		}
-		delay(1000);
+		return 0;
 	}
-	return 0;
 }
